77e3372a8fbf712220501a797cf306e1078f9e28
Pasha Tatashin <pasha.tatashin@soleen.com>
2024-03-11 16:46:37 +0000

    task_stack.h: Add stack_not_used() support for dynamic stack
    CONFIG_DEBUG_STACK_USAGE is enabled by default on most architectures.
    
    Its purpose is to determine and print the maximum stack depth on
    thread exit.
    
    The way it works, is it starts from the buttom of the stack and
    searches the first non-zero word in the stack. With dynamic stack it
    does not work very well, as it means it faults every pages in every
    stack.
    
    Instead, add a specific version of stack_not_used() for dynamic stacks
    where instead of starting from the buttom of the stack, we start from
    the last page mapped in the stack.
    
    In addition to not doing uncessary page faulting, this search is
    optimized by skipping search through zero pages.
    
    Also, because dynamic stack does not end with MAGIC_NUMBER, there is
    no need to skeep the buttom most word in the stack.
    
    Signed-off-by: Pasha Tatashin <pasha.tatashin@soleen.com>
    Link: https://lore.kernel.org/20240311164638.2015063-14-pasha.tatashin@soleen.com
    [Rebased, Kasan oneliner needed preserving, rewrote a bit due to bugs]
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- kernel/exit.c
+++ kernel/exit.c
@@ -71,6 +71,7 @@
 #include <linux/unwind_deferred.h>
 #include <linux/uaccess.h>
 #include <linux/pidfs.h>
+#include <linux/vmalloc.h>
 
 #include <uapi/linux/wait.h>
 
@@ -792,6 +793,20 @@ unsigned long stack_not_used(struct task_struct *p)
 	return (unsigned long)end_of_stack(p) - (unsigned long)n;
 }
 #else /* !CONFIG_STACK_GROWSUP */
+#ifdef CONFIG_DYNAMIC_STACK
+unsigned long stack_not_used(struct task_struct *p)
+{
+	struct vm_struct *vm_area = p->stack_vm_area;
+	unsigned long alloc_size = vm_area->nr_pages << PAGE_SHIFT;
+	unsigned long stack = (unsigned long)p->stack;
+	unsigned long *n = (unsigned long *)(stack + THREAD_SIZE - alloc_size);
+
+	while (!*n)
+		n++;
+
+	return (unsigned long)n - stack;
+}
+#else
 unsigned long stack_not_used(struct task_struct *p)
 {
 	unsigned long *n = end_of_stack(p);
@@ -802,6 +817,7 @@ unsigned long stack_not_used(struct task_struct *p)
 
 	return (unsigned long)n - (unsigned long)end_of_stack(p);
 }
+#endif /* CONFIG_DYNAMIC_STACK */
 #endif /* CONFIG_STACK_GROWSUP */
 
 /* Count the maximum pages reached in kernel stacks */
