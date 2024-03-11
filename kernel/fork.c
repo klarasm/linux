55514a1715bf71497392f655c9e35013510e22b3
Pasha Tatashin <pasha.tatashin@soleen.com>
2024-03-11 16:46:38 +0000

    fork: Dynamic Kernel Stack accounting
    Add an accounting of amount of stack pages that has been faulted is
    currently in use.
    
    Example use case:
      $ cat /proc/vmstat | grep stack
      nr_kernel_stack 18684
      nr_dynamic_stacks_faults 156
    
    The above shows that the kernel stacks use total 18684KiB, out of which
    156KiB were faulted in.
    
    Given that the pre-allocated stacks are 4KiB, we can determine the total
    number of tasks:
    
    tasks = (nr_kernel_stack - nr_dynamic_stacks_faults) / 4 = 4632.
    
    The amount of kernel stack memory without dynamic stack on this machine
    woud be:
    
    4632 * 16 KiB = 74,112 KiB
    
    Therefore, in this example dynamic stacks save: 55,428 KiB
    
    Signed-off-by: Pasha Tatashin <pasha.tatashin@soleen.com>
    Link: https://lore.kernel.org/20240311164638.2015063-15-pasha.tatashin@soleen.com
    [Rebased]
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- kernel/fork.c
+++ kernel/fork.c
@@ -359,6 +359,9 @@ void dynamic_stack_refill_pages(void)
 
 		mod_lruvec_page_state(page, NR_KERNEL_STACK_KB,
 				      PAGE_SIZE / 1024);
+		mod_lruvec_page_state(page,
+				      NR_DYNAMIC_STACKS_FAULTS_KB,
+				      PAGE_SIZE / 1024);
 
 		page = alloc_pages(GFP_VMAP_STACK, 0);
 		if (unlikely(!page))
@@ -675,9 +678,17 @@ static void account_kernel_stack(struct task_struct *tsk, int account)
 		struct vm_struct *vm_area = task_stack_vm_area(tsk);
 		int i;
 
-		for (i = 0; i < vm_area->nr_pages; i++)
+		for (i = 0; i < vm_area->nr_pages; i++) {
 			mod_lruvec_page_state(vm_area->pages[i], NR_KERNEL_STACK_KB,
 					      account * (PAGE_SIZE / 1024));
+#ifdef CONFIG_DYNAMIC_STACK
+			if (i >= THREAD_PREALLOC_PAGES) {
+				mod_lruvec_page_state(vm_area->pages[i],
+						      NR_DYNAMIC_STACKS_FAULTS_KB,
+						      account * (PAGE_SIZE / 1024));
+			}
+#endif
+		}
 	} else {
 		void *stack = task_stack_page(tsk);
 
