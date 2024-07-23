4f432a95b58da0beea1fb9e2e74530c3bbf1ba40
Klara Modin <klarasmodin@gmail.com>
2024-07-23 13:02:40 +0200

    ethernet: use substruct for multi_msi_en in dwmac-spacemit
    Signed-off-by: Klara Modin <klarasmodin@gmail.com>

--- drivers/net/ethernet/stmicro/stmmac/dwmac-spacemit.c
+++ drivers/net/ethernet/stmicro/stmmac/dwmac-spacemit.c
@@ -271,7 +271,7 @@ static int spacemit_dwmac_probe(struct platform_device *pdev)
 				      plat_dat->multicast_filter_bins);
 	plat_dat->has_gmac = 1;
 	plat_dat->pmt = 1;
-	plat_dat->multi_msi_en = 0;
+	plat_dat->dma_cfg->multi_msi_en = 0;
 	plat_dat->phy_addr = spacemit_plat_dat->data->addr_width;
 	plat_dat->enh_desc = 1;
 
