8a50d89f967bcd7822b9982cfe0e263898c97769
Pali Rohár <pali@kernel.org>
2024-10-06 19:44:50 +0200

    cifs: Remove unicode parameter from parse_reparse_point() function
    This parameter is always true, so remove it and also remove dead code which
    is never called (for all false code paths).
    
    Signed-off-by: Pali Rohár <pali@kernel.org>
    Signed-off-by: Steve French <stfrench@microsoft.com>

--- fs/smb/client/smb2proto.h
+++ fs/smb/client/smb2proto.h
@@ -113,7 +113,7 @@ extern int smb3_query_mf_symlink(unsigned int xid, struct cifs_tcon *tcon,
 			  unsigned int *pbytes_read);
 int smb2_fix_symlink_target_type(char **target, bool directory, struct cifs_sb_info *cifs_sb);
 int smb2_parse_native_symlink(char **target, const char *buf, unsigned int len,
-			      bool unicode, bool relative,
+			      bool relative,
 			      const char *full_path,
 			      struct cifs_sb_info *cifs_sb);
 int smb2_parse_symlink_response(struct cifs_sb_info *cifs_sb,
