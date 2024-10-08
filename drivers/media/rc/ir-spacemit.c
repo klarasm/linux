9a7e96e0764887c271910315203ceea67b80b76f
Klara Modin <klarasmodin@gmail.com>
2024-10-08 16:56:03 +0200

    media/rc: add missing linux/of_device.h header to ir-spacemit.c
    Signed-off-by: Klara Modin <klarasmodin@gmail.com>

--- drivers/media/rc/ir-spacemit.c
+++ drivers/media/rc/ir-spacemit.c
@@ -8,6 +8,7 @@
 #include <linux/interrupt.h>
 #include <linux/module.h>
 #include <linux/of.h>
+#include <linux/of_device.h>
 #include <linux/of_platform.h>
 #include <linux/reset.h>
 #include <linux/platform_device.h>
