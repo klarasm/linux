ed817a235c2a9a91e4649a09c14e57b2e8a02e48
Lorenzo Stoakes <lorenzo.stoakes@oracle.com>
2024-10-17 21:42:37 +0100

    mm: madvise: implement lightweight guard page mechanism
    Implement a new lightweight guard page feature, that is regions of userland
    virtual memory that, when accessed, cause a fatal signal to arise.
    
    Currently users must establish PROT_NONE ranges to achieve this.
    
    However this is very costly memory-wise - we need a VMA for each and every
    one of these regions AND they become unmergeable with surrounding VMAs.
    
    In addition repeated mmap() calls require repeated kernel context switches
    and contention of the mmap lock to install these ranges, potentially also
    having to unmap memory if installed over existing ranges.
    
    The lightweight guard approach eliminates the VMA cost altogether - rather
    than establishing a PROT_NONE VMA, it operates at the level of page table
    entries - poisoning PTEs such that accesses to them cause a fault followed
    by a SIGSGEV signal being raised.
    
    This is achieved through the PTE marker mechanism, which a previous commit
    in this series extended to permit this to be done, installed via the
    generic page walking logic, also extended by a prior commit for this
    purpose.
    
    These poison ranges are established with MADV_GUARD_POISON, and if the
    range in which they are installed contain any existing mappings, they will
    be zapped, i.e. free the range and unmap memory (thus mimicking the
    behaviour of MADV_DONTNEED in this respect).
    
    Any existing poison entries will be left untouched. There is no nesting of
    poisoned pages.
    
    Poisoned ranges are NOT cleared by MADV_DONTNEED, as this would be rather
    unexpected behaviour, but are cleared on process teardown or unmapping of
    memory ranges.
    
    Ranges can have the poison property removed by MADV_GUARD_UNPOISON -
    'remedying' the poisoning. The ranges over which this is applied, should
    they contain non-poison entries, will be untouched, only poison entries
    will be cleared.
    
    We permit this operation on anonymous memory only, and only VMAs which are
    non-special, non-huge and not mlock()'d (if we permitted this we'd have to
    drop locked pages which would be rather counterintuitive).
    
    Suggested-by: Vlastimil Babka <vbabka@suze.cz>
    Suggested-by: Jann Horn <jannh@google.com>
    Suggested-by: David Hildenbrand <david@redhat.com>
    Signed-off-by: Lorenzo Stoakes <lorenzo.stoakes@oracle.com>

--- arch/parisc/include/uapi/asm/mman.h
+++ arch/parisc/include/uapi/asm/mman.h
@@ -75,6 +75,9 @@
 #define MADV_HWPOISON     100		/* poison a page for testing */
 #define MADV_SOFT_OFFLINE 101		/* soft offline page for testing */
 
+#define MADV_GUARD_POISON 102		/* fatal signal on access to range */
+#define MADV_GUARD_UNPOISON 103		/* revoke guard poisoning */
+
 /* compatibility flags */
 #define MAP_FILE	0
 
