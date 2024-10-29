c80e6e9de6aec2a9e9c7671872b75f3fcae810a7
Frederic Weisbecker <frederic@kernel.org>
2024-10-29 13:54:43 +0100

    clockevents: Shutdown and unregister current clockevents at CPUHP_AP_TICK_DYING
    The way the clockevent devices are finally stopped while a CPU is
    offlining is currently chaotic. The layout being by order:
    
    1) tick_sched_timer_dying() stops the tick and the underlying clockevent
      but only for oneshot case. The periodic tick and its related
      clockevent still runs.
    
    2) tick_broadcast_offline() detaches and stops the per-cpu oneshot
      broadcast and append it to the released list.
    
    3) Some individual clockevent drivers stop the clockevents (a second time if
      the tick is oneshot)
    
    4) Once the CPU is dead, a control CPU remotely detaches and stops
      (a 3rd time if oneshot mode) the CPU clockevent and adds it to the
      released list.
    
    5) The released list containing the broadcast device released on step 2)
       and the remotely detached clockevent from step 4) are unregistered.
    
    These random events can be factorized if the current clockevent is
    detached and stopped by the dying CPU at the generic layer, that is
    from the dying CPU:
    
    a) Stop the tick
    b) Stop/detach the underlying per-cpu oneshot broadcast clockevent
    c) Stop/detach the underlying clockevent
    d) Release / unregister the clockevents from b) and c)
    e) Release / unregister the remaining clockevents from the dying CPU.
       This part could be performed by the dying CPU
    
    This way the drivers and the tick layer don't need to care about
    clockevent operations during cpuhotplug down. This also unifies the tick
    behaviour on offline CPUs between oneshot and periodic modes, avoiding
    offline ticks altogether for sanity.
    
    Adopt the simplification and verify no further clockevent can be
    registered for the dying CPU after the final release.
    
    Signed-off-by: Frederic Weisbecker <frederic@kernel.org>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Link: https://lore.kernel.org/all/20241029125451.54574-3-frederic@kernel.org

--- kernel/time/clockevents.c
+++ kernel/time/clockevents.c
@@ -452,6 +452,9 @@ void clockevents_register_device(struct clock_event_device *dev)
 {
 	unsigned long flags;
 
+	if (WARN_ON_ONCE(cpu_is_offline(raw_smp_processor_id())))
+		return;
+
 	/* Initialize state to DETACHED */
 	clockevent_set_state(dev, CLOCK_EVT_STATE_DETACHED);
 
@@ -618,39 +621,30 @@ void clockevents_resume(void)
 
 #ifdef CONFIG_HOTPLUG_CPU
 
-# ifdef CONFIG_GENERIC_CLOCKEVENTS_BROADCAST
 /**
- * tick_offline_cpu - Take CPU out of the broadcast mechanism
+ * tick_offline_cpu - Shutdown all clock events related
+ *                    to this CPU and take it out of the
+ *                    broadcast mechanism.
  * @cpu:	The outgoing CPU
  *
- * Called on the outgoing CPU after it took itself offline.
+ * Called by the dying CPU during teardown.
  */
 void tick_offline_cpu(unsigned int cpu)
 {
-	raw_spin_lock(&clockevents_lock);
-	tick_broadcast_offline(cpu);
-	raw_spin_unlock(&clockevents_lock);
-}
-# endif
-
-/**
- * tick_cleanup_dead_cpu - Cleanup the tick and clockevents of a dead cpu
- * @cpu:	The dead CPU
- */
-void tick_cleanup_dead_cpu(int cpu)
-{
 	struct clock_event_device *dev, *tmp;
-	unsigned long flags;
 
-	raw_spin_lock_irqsave(&clockevents_lock, flags);
+	raw_spin_lock(&clockevents_lock);
 
+	tick_broadcast_offline(cpu);
 	tick_shutdown(cpu);
+
 	/*
 	 * Unregister the clock event devices which were
-	 * released from the users in the notify chain.
+	 * released above.
 	 */
 	list_for_each_entry_safe(dev, tmp, &clockevents_released, list)
 		list_del(&dev->list);
+
 	/*
 	 * Now check whether the CPU has left unused per cpu devices
 	 */
@@ -662,7 +656,8 @@ void tick_cleanup_dead_cpu(int cpu)
 			list_del(&dev->list);
 		}
 	}
-	raw_spin_unlock_irqrestore(&clockevents_lock, flags);
+
+	raw_spin_unlock(&clockevents_lock);
 }
 #endif
 
