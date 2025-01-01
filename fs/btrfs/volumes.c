b336adec62d1029a080930706972ee4ff45b2535
Anand Jain <anand.jain@oracle.com>
2025-01-02 02:06:38 +0800

    btrfs: enable RAID1 balancing configuration via modprobe parameter
    This update allows configuring the `raid1-balancing` methods using a
    modprobe parameter when experimental mode CONFIG_BTRFS_EXPERIMENTAL
    is enabled.
    
    Examples:
    
    - Set the RAID1 balancing method to round-robin with a custom
    `min_contiguous_read` of 4k:
      $ modprobe btrfs raid1-balancing=round-robin:4096
    
    - Set the round-robin balancing method with the default
    `min_contiguous_read`:
      $ modprobe btrfs raid1-balancing=round-robin
    
    - Set the `devid` balancing method, defaulting to the latest
    device:
      $ modprobe btrfs raid1-balancing=devid
    
    Signed-off-by: Anand Jain <anand.jain@oracle.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/volumes.c
+++ fs/btrfs/volumes.c
@@ -1300,6 +1300,7 @@ static int open_fs_devices(struct btrfs_fs_devices *fs_devices,
 	struct btrfs_device *device;
 	struct btrfs_device *latest_dev = NULL;
 	struct btrfs_device *tmp_device;
+	s64 __maybe_unused value = 0;
 	int ret = 0;
 
 	/* Initialize the in-memory record of filesystem read count */
@@ -1334,10 +1335,21 @@ static int open_fs_devices(struct btrfs_fs_devices *fs_devices,
 	fs_devices->latest_dev = latest_dev;
 	fs_devices->total_rw_bytes = 0;
 	fs_devices->chunk_alloc_policy = BTRFS_CHUNK_ALLOC_REGULAR;
-	fs_devices->read_policy = BTRFS_READ_POLICY_PID;
 #ifdef CONFIG_BTRFS_EXPERIMENTAL
 	fs_devices->rr_min_contiguous_read = BTRFS_DEFAULT_RR_MIN_CONTIGUOUS_READ;
 	fs_devices->read_devid = latest_dev->devid;
+	fs_devices->read_policy =
+		btrfs_read_policy_to_enum(btrfs_get_raid1_balancing(), &value);
+	if (fs_devices->read_policy == BTRFS_READ_POLICY_RR)
+		fs_devices->fs_stats = true;
+	if (value) {
+		if (fs_devices->read_policy == BTRFS_READ_POLICY_RR)
+			fs_devices->rr_min_contiguous_read = value;
+		if (fs_devices->read_policy == BTRFS_READ_POLICY_DEVID)
+			fs_devices->read_devid = value;
+	}
+#else
+	fs_devices->read_policy = BTRFS_READ_POLICY_PID;
 #endif
 
 	return 0;
