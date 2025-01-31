8ff6fec20c7193db470b107072c86fec17eb7973
Sergey Senozhatsky <senozhatsky@chromium.org>
2025-01-31 18:06:16 +0900

    zram: add might_sleep to zcomp API
    Explicitly state that zcomp compress/decompress must be
    called from non-atomic context.
    
    Signed-off-by: Sergey Senozhatsky <senozhatsky@chromium.org>

--- drivers/block/zram/zcomp.c
+++ drivers/block/zram/zcomp.c
@@ -185,6 +185,7 @@ int zcomp_compress(struct zcomp *comp, struct zcomp_strm *zstrm,
 	};
 	int ret;
 
+	might_sleep();
 	ret = comp->ops->compress(comp->params, &zstrm->ctx, &req);
 	if (!ret)
 		*dst_len = req.dst_len;
@@ -201,6 +202,7 @@ int zcomp_decompress(struct zcomp *comp, struct zcomp_strm *zstrm,
 		.dst_len = PAGE_SIZE,
 	};
 
+	might_sleep();
 	return comp->ops->decompress(comp->params, &zstrm->ctx, &req);
 }
 
