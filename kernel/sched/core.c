484aecc04f9a83a874998dce7bd8380c2dd0b876
Jinjie Ruan <ruanjinjie@huawei.com>
2025-02-13 21:00:00 +0800

    entry: Split generic entry into generic exception and syscall entry
    Currently CONFIG_GENERIC_ENTRY enables both the generic exception
    entry logic and the generic syscall entry logic, which are otherwise
    loosely coupled.
    
    Introduce separate config options for these so that archtiectures can
    select the two independently. This will make it easier for
    architectures to migrate to generic entry code.
    
    ----
    v6:
    - Udapte the commit message.
    - Have this before the arm64 changes.
    - Make GENERIC_SYSCALL depends on GENERIC_IRQ_ENTRY.
    
    Suggested-by: Mark Rutland <mark.rutland@arm.com>
    Signed-off-by: Jinjie Ruan <ruanjinjie@huawei.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -68,8 +68,8 @@
 #include <linux/workqueue_api.h>
 
 #ifdef CONFIG_PREEMPT_DYNAMIC
-# ifdef CONFIG_GENERIC_ENTRY
-#  include <linux/entry-common.h>
+# ifdef CONFIG_GENERIC_IRQ_ENTRY
+#  include <linux/irq-entry-common.h>
 # endif
 #endif
 
@@ -7417,8 +7417,8 @@ EXPORT_SYMBOL(__cond_resched_rwlock_write);
 
 #ifdef CONFIG_PREEMPT_DYNAMIC
 
-#ifdef CONFIG_GENERIC_ENTRY
-#include <linux/entry-common.h>
+#ifdef CONFIG_GENERIC_IRQ_ENTRY
+#include <linux/irq-entry-common.h>
 #endif
 
 /*
