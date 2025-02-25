53b18ec2bef38881c9dc030a190fc47d1420621a
Linus Walleij <linus.walleij@linaro.org>
2025-02-25 10:55:12 +0100

    ARM: entry: Create irqentry calls from kernel mode
    This adds a few calls to C when exceptions enter/exit from
    the kernel itself, so these can be used for generic entry.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Link: https://lore.kernel.org/r/20250225-arm-generic-entry-v5-25-2f02313653e5@linaro.org

--- arch/arm/include/asm/entry.h
+++ arch/arm/include/asm/entry.h
@@ -12,5 +12,7 @@ long syscall_enter_from_user_mode(struct pt_regs *regs, long);
 void syscall_exit_to_user_mode(struct pt_regs *regs);
 void irqentry_enter_from_user_mode(struct pt_regs *regs);
 void irqentry_exit_to_user_mode(struct pt_regs *regs);
+void irqentry_enter_from_kernel_mode(struct pt_regs *regs);
+void irqentry_exit_to_kernel_mode(struct pt_regs *regs);
 
 #endif /* __ASM_ENTRY_H__ */
