c712a9e3e65d264fcd9fee569c4d219d19d58640
Linus Walleij <linus.walleij@linaro.org>
2025-02-25 10:55:02 +0100

    ARM: entry: Separate call path for syscall SWI entry
    The syscalls (SWIs, software interrupts) are deviating from how
    any other interrupts are handled as they enable the IRQs again
    while processing the syscall, while "hard" IRQs disable all
    interrupts until they are handled.
    
    Break out syscall_enter_from_user_mode() into its own function
    and call it instead of irqentry_enter_from_user_mode().
    
    As we are moving toward generic entry, we use the signature
    from the generic function.
    
    As the generic function requires the syscall number to be
    determined, we move the call down below the code that
    figures out the syscall number, the only practical effect
    should be that interrupts are re-enabled a few instructions
    later.
    
    As we move the trace_hardirqs_on/off() calls into C, we can
    just get rid of the helper macro usr_entry_enter again and
    call asm_irqentry_enter_from_user_mode directly.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Link: https://lore.kernel.org/r/20250225-arm-generic-entry-v5-15-2f02313653e5@linaro.org

--- arch/arm/kernel/entry.c
+++ arch/arm/kernel/entry.c
@@ -1,15 +1,29 @@
 // SPDX-License-Identifier: GPL-2.0
 #include <asm/entry.h>
 #include <linux/context_tracking.h>
+#include <linux/irqflags.h>
+
+long syscall_enter_from_user_mode(struct pt_regs *regs, long syscall)
+{
+	trace_hardirqs_on();
+	local_irq_enable();
+	/* This context tracking call has inverse naming */
+	user_exit_callable();
+
+	/* This will optionally be modified later */
+	return syscall;
+}
 
 noinstr void irqentry_enter_from_user_mode(struct pt_regs *regs)
 {
+	trace_hardirqs_off();
 	/* This context tracking call has inverse naming */
 	user_exit_callable();
 }
 
 noinstr void irqentry_exit_to_user_mode(struct pt_regs *regs)
 {
+	trace_hardirqs_on();
 	/* This context tracking call has inverse naming */
 	user_enter_callable();
 }
