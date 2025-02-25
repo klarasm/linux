832f87d6a880f6d3be2b0d536db34bd1dc375754
Linus Walleij <linus.walleij@linaro.org>
2025-02-25 10:55:18 +0100

    ARM: entry: Reimplement local restart in C
    The former local restart hack to restart syscalls inside
    the kernel if we get restart signals while processing a
    system call was deleted when converting the architecture
    to generic entry.
    
    This makes strace tests fail so the hack is necessary.
    
    Now, after the conversion to generic entry, restore the
    order by reimplementing this with two TIF flags that
    help us to issue system call restarts immediately in the
    kernel.
    
    This is essentially a reimplementation of commit 81783786d5cf
    "ARM: 7473/1: deal with handlerless restarts without leaving
    the kernel" from 2012, but in C, on top of generic entry.
    
    Link: http://lists.infradead.org/pipermail/linux-arm-kernel/2012-June/104733.html
    Link: https://lore.kernel.org/all/1340377626-17075-1-git-send-email-will.deacon@arm.com/
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Link: https://lore.kernel.org/r/20250225-arm-generic-entry-v5-31-2f02313653e5@linaro.org

--- arch/arm/kernel/syscall.c
+++ arch/arm/kernel/syscall.c
@@ -11,6 +11,7 @@ __visible void invoke_syscall(void *table, struct pt_regs *regs, int scno)
 {
 	int ret;
 
+local_restart:
 	scno = syscall_enter_from_user_mode(regs, scno);
 	/* When tracing syscall -1 means "skip syscall" */
 	if (scno < 0) {
@@ -34,4 +35,25 @@ exit_save:
 	syscall_set_return_value(current, regs, 0, ret);
 
 	syscall_exit_to_user_mode(regs);
+
+	/*
+	 * Handle local restart: this means that when generic entry
+	 * calls arch_do_signal_or_restart() because a signal to
+	 * restart the syscall arrived while processing a system call,
+	 * we set these flags for the thread so that we don't even
+	 * exit the kernel, we just restart right here and clear
+	 * the restart condition.
+	 *
+	 * This is done because of signal race issues on ARM.
+	 */
+	if (test_thread_flag(TIF_LOCAL_RESTART)) {
+		if (test_thread_flag(TIF_LOCAL_RESTART_BLOCK)) {
+			scno = __NR_restart_syscall - __NR_SYSCALL_BASE;
+			/* Make this change visible to tracers */
+			task_thread_info(current)->abi_syscall = scno;
+			clear_thread_flag(TIF_LOCAL_RESTART_BLOCK);
+		}
+		clear_thread_flag(TIF_LOCAL_RESTART);
+		goto local_restart;
+	}
 }
