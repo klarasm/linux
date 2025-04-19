a4061159dc8824137a92b968cebebed3092bf3d5
Linus Walleij <linus.walleij@linaro.org>
2025-04-20 00:36:00 +0200

    ARM: entry: Create user_mode_enter/exit
    The callbacks to the context tracking will be repurposed
    for several uses that are needed on the IRQ transition
    to/from userspace.
    
    Rename the macro, establish call sites in C calling into
    the context tracking following the corresponding generic
    entry function prototypes, despite the assembly macro
    names become a bit long this makes it clear to readers
    exactly what is going on and where this call will go.
    
    Drop the ifdefs pertaining to context tracking from
    the macro. The C calls we will use have stubs that will
    compile these out anyway.
    
    The inversion of the signature of the context tracking
    calls are especially confusing since the generic entry
    uses the reverse semantics: *enter from* user mode (to
    kernel mode) and *exit to* user mode (from kernel mode)
    instead of the other way around as the old context tracker
    code user_exit_callable() and user_enter_callable()
    which have inverted semantics.
    
    Acked-by: Thomas Gleixner <tglx@linutronix.de>
    Acked-by: Paul E. McKenney <paulmck@kernel.org>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Link: https://lore.kernel.org/r/20250420-arm-generic-entry-v6-13-95f1fcdfeeb2@linaro.org

---
+++ arch/arm/include/asm/entry.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+#ifndef __ASM_ENTRY_H__
+#define __ASM_ENTRY_H__
+
+struct pt_regs;
+
+/*
+ * These are copies of generic entry headers so we can transition
+ * to generic entry once they are semantically equivalent.
+ */
+void irqentry_enter_from_user_mode(struct pt_regs *regs);
+void irqentry_exit_to_user_mode(struct pt_regs *regs);
+
+#endif /* __ASM_ENTRY_H__ */
