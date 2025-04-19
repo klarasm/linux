3a0f42f416e01a3cf2072b13785824fb0e18be04
Linus Walleij <linus.walleij@linaro.org>
2025-04-20 00:35:57 +0200

    ARM: entry: move all tracing invocation to C
    The traced invocation of syscall is rewritten in C, moving
    over also the call to syscall_trace_exit() to C.
    
    To do this we cannot have invoke_syscall_trace_asm() be a
    leaf call, which is the mechanism we hithereto relied on
    to make sure the stack is pulled back to the state we were
    at before we called out to the code written in C.
    
    Push the registers potentially used by C on the stack, and
    make a copy of the two stack-pushed syscall arguments
    on the top of the stack before invoking the syscall and
    then drop these copied arguments and pop back the
    registers before returning from invoke_syscall_trace_asm().
    
    As we are now calling from and returning to C, we no
    longer need to pass the regs pointer around so drop it.
    
    Acked-by: Thomas Gleixner <tglx@linutronix.de>
    Acked-by: Paul E. McKenney <paulmck@kernel.org>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Link: https://lore.kernel.org/r/20250420-arm-generic-entry-v6-10-95f1fcdfeeb2@linaro.org

--- arch/arm/kernel/syscall.c
+++ arch/arm/kernel/syscall.c
@@ -18,23 +18,35 @@ __visible int invoke_syscall(void *table, struct pt_regs *regs, int scno, void *
 	return sys_ni_syscall();
 }
 
-int invoke_syscall_trace_asm(void *table, struct pt_regs *regs, int scno, void *retp);
+int invoke_syscall_trace_asm(void *table, struct pt_regs *regs, int scno);
 __ADDRESSABLE(invoke_syscall_trace_asm);
 
-__visible int invoke_syscall_trace(void *table, struct pt_regs *regs, void *retp)
+__visible void invoke_syscall_trace(void *table, struct pt_regs *regs)
 {
 	int scno;
+	int ret;
 
 	scno = syscall_trace_enter(regs);
 	if (scno == -1)
-		return -1;
+		goto trace_exit_nosave;
 
-	if (scno < NR_syscalls)
-		/* Doing this with return makes sure the stack gets pop:ed */
-		return invoke_syscall_trace_asm(table, regs, scno, retp);
+	if (scno < NR_syscalls) {
+		ret = invoke_syscall_trace_asm(table, regs, scno);
+		goto trace_exit_save;
+	}
 
-	if (scno >= __ARM_NR_BASE)
-		return arm_syscall(scno, regs);
+	if (scno >= __ARM_NR_BASE) {
+		ret = arm_syscall(scno, regs);
+		goto trace_exit_save;
+	}
 
-	return sys_ni_syscall();
+	ret = sys_ni_syscall();
+
+trace_exit_save:
+	/* Save return value from syscall */
+	regs->ARM_r0 = ret;
+
+trace_exit_nosave:
+	local_irq_enable();
+	syscall_trace_exit(regs);
 }
