8619c4bc05c275b451d47645f99d681b6b55497e
Linus Walleij <linus.walleij@linaro.org>
2025-04-20 00:36:09 +0200

    ARM: entry: Move work processing to C
    At the end of a syscall software interrupt and every time we exit
    an interrupt handler we check if there is work to do, i.e. if the
    current thread (userspace or kernel daemon) has set the
    _TIF_SYSCALL_WORK flag.
    
    Move this processing over to the C entry code.
    
    Acked-by: Thomas Gleixner <tglx@linutronix.de>
    Acked-by: Paul E. McKenney <paulmck@kernel.org>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Link: https://lore.kernel.org/r/20250420-arm-generic-entry-v6-22-95f1fcdfeeb2@linaro.org

--- arch/arm/kernel/entry.c
+++ arch/arm/kernel/entry.c
@@ -1,9 +1,15 @@
 // SPDX-License-Identifier: GPL-2.0
 #include <asm/entry.h>
+#include <asm/ptrace.h>
 #include <asm/signal.h>
 #include <linux/context_tracking.h>
 #include <linux/irqflags.h>
 #include <linux/rseq.h>
+
+static inline bool has_syscall_work(unsigned long flags)
+{
+	return unlikely(flags & _TIF_SYSCALL_WORK);
+}
 
 long syscall_enter_from_user_mode(struct pt_regs *regs, long syscall)
 {
@@ -18,8 +24,12 @@ long syscall_enter_from_user_mode(struct pt_regs *regs, long syscall)
 
 void syscall_exit_to_user_mode(struct pt_regs *regs)
 {
+	unsigned long flags = read_thread_flags();
+
 	rseq_syscall(regs);
 	local_irq_disable();
+	if (has_syscall_work(flags))
+		do_work_pending(regs, flags);
 }
 
 noinstr void irqentry_enter_from_user_mode(struct pt_regs *regs)
