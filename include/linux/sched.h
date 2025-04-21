e4f588097453c81cc457696077733fa813ec005d
Peter Zijlstra <peterz@infradead.org>
2025-04-21 11:24:26 +0800

    sched: Cache aware load-balancing
    Hi all,
    
    One of the many things on the eternal todo list has been finishing the
    below hackery.
    
    It is an attempt at modelling cache affinity -- and while the patch
    really only targets LLC, it could very well be extended to also apply to
    clusters (L2). Specifically any case of multiple cache domains inside a
    node.
    
    Anyway, I wrote this about a year ago, and I mentioned this at the
    recent OSPM conf where Gautham and Prateek expressed interest in playing
    with this code.
    
    So here goes, very rough and largely unproven code ahead :-)
    
    It applies to current tip/master, but I know it will fail the __percpu
    validation that sits in -next, although that shouldn't be terribly hard
    to fix up.
    
    As is, it only computes a CPU inside the LLC that has the highest recent
    runtime, this CPU is then used in the wake-up path to steer towards this
    LLC and in task_hot() to limit migrations away from it.
    
    More elaborate things could be done, notably there is an XXX in there
    somewhere about finding the best LLC inside a NODE (interaction with
    NUMA_BALANCING).
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lore.kernel.org/r/391c48836585786ed32d66df9534366459684383.1745199017.git.yu.c.chen@intel.com

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1407,6 +1407,10 @@ struct task_struct {
 	unsigned long			numa_pages_migrated;
 #endif /* CONFIG_NUMA_BALANCING */
 
+#ifdef CONFIG_SCHED_CACHE
+	struct callback_head		cache_work;
+#endif
+
 #ifdef CONFIG_RSEQ
 	struct rseq __user *rseq;
 	u32 rseq_len;
