bc34e0e34c7ca5aa0a119163e6de3e117353c118
Tvrtko Ursulin <tvrtko.ursulin@igalia.com>
2025-04-25 11:20:27 +0100

    drm/sched: Move run queue related code into a separate file
    Lets move all the code dealing with struct drm_sched_rq into a separate
    compilation unit. Advantage being sched_main.c is left with a clearer set
    of responsibilities.
    
    Signed-off-by: Tvrtko Ursulin <tvrtko.ursulin@igalia.com>
    Cc: Christian König <christian.koenig@amd.com>
    Cc: Danilo Krummrich <dakr@kernel.org>
    Cc: Matthew Brost <matthew.brost@intel.com>
    Cc: Philipp Stanner <phasta@kernel.org>
    Link: https://lore.kernel.org/r/20250425102034.85133-10-tvrtko.ursulin@igalia.com

--- drivers/gpu/drm/scheduler/sched_internal.h
+++ drivers/gpu/drm/scheduler/sched_internal.h
@@ -10,8 +10,15 @@ extern int drm_sched_policy;
 #define DRM_SCHED_POLICY_RR    0
 #define DRM_SCHED_POLICY_FIFO  1
 
+bool drm_sched_can_queue(struct drm_gpu_scheduler *sched,
+			 struct drm_sched_entity *entity);
 void drm_sched_wakeup(struct drm_gpu_scheduler *sched);
 
+void drm_sched_rq_init(struct drm_gpu_scheduler *sched,
+		       struct drm_sched_rq *rq);
+struct drm_sched_entity *
+drm_sched_rq_select_entity(struct drm_gpu_scheduler *sched,
+			   struct drm_sched_rq *rq);
 struct drm_gpu_scheduler *
 drm_sched_rq_add_entity(struct drm_sched_entity *entity, ktime_t ts);
 void drm_sched_rq_remove_entity(struct drm_sched_rq *rq,
