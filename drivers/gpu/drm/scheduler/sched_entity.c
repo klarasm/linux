cc44228cea6124e987b7f9329c4d2f1e4ccdd2e3
Tvrtko Ursulin <tvrtko.ursulin@igalia.com>
2025-04-25 11:20:29 +0100

    drm/sched: Account entity GPU time
    To implement fair scheduling we need a view into the GPU time consumed by
    entities. Problem we have is that jobs and entities objects have decoupled
    lifetimes, where at the point we have a view into accurate GPU time, we
    cannot link back to the entity any longer.
    
    Solve this by adding a light weight entity stats object which is reference
    counted by both entity and the job and hence can safely be used from
    either side.
    
    With that, the only other thing we need is to add a helper for adding the
    job's GPU time into the respective entity stats object, and call it once
    the accurate GPU time has been calculated.
    
    Signed-off-by: Tvrtko Ursulin <tvrtko.ursulin@igalia.com>
    Cc: Christian König <christian.koenig@amd.com>
    Cc: Danilo Krummrich <dakr@kernel.org>
    Cc: Matthew Brost <matthew.brost@intel.com>
    Cc: Philipp Stanner <phasta@kernel.org>
    Link: https://lore.kernel.org/r/20250425102034.85133-12-tvrtko.ursulin@igalia.com

--- drivers/gpu/drm/scheduler/sched_entity.c
+++ drivers/gpu/drm/scheduler/sched_entity.c
@@ -32,6 +32,29 @@
 
 #include "gpu_scheduler_trace.h"
 
+
+void drm_sched_entity_stats_release(struct kref *kref)
+{
+	struct drm_sched_entity_stats *stats =
+		container_of(kref, typeof(*stats), kref);
+
+	kfree(stats);
+}
+
+static struct drm_sched_entity_stats *drm_sched_entity_stats_alloc(void)
+{
+	struct drm_sched_entity_stats *stats;
+
+	stats = kzalloc(sizeof(*stats), GFP_KERNEL);
+	if (!stats)
+		return NULL;
+
+	kref_init(&stats->kref);
+	spin_lock_init(&stats->lock);
+
+	return stats;
+}
+
 /**
  * drm_sched_entity_init - Init a context entity used by scheduler when
  * submit to HW ring.
@@ -65,6 +88,11 @@ int drm_sched_entity_init(struct drm_sched_entity *entity,
 		return -EINVAL;
 
 	memset(entity, 0, sizeof(struct drm_sched_entity));
+
+	entity->stats = drm_sched_entity_stats_alloc();
+	if (!entity->stats)
+		return -ENOMEM;
+
 	INIT_LIST_HEAD(&entity->list);
 	entity->rq = NULL;
 	entity->guilty = guilty;
@@ -337,6 +365,7 @@ void drm_sched_entity_fini(struct drm_sched_entity *entity)
 
 	dma_fence_put(rcu_dereference_check(entity->last_scheduled, true));
 	RCU_INIT_POINTER(entity->last_scheduled, NULL);
+	drm_sched_entity_stats_put(entity->stats);
 }
 EXPORT_SYMBOL(drm_sched_entity_fini);
 
