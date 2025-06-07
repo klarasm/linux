91ab841cf9fe96a263d334e4969ce8e0a82e6566
Andrew Morton <akpm@linux-foundation.org>
2025-06-07 14:32:35 -0700

    fs-proc-task_mmu-add-vm_shadow_stack-for-arm64-when-support-gcs-fix
    simplify ifdeffery
    
    Cc: Andrii Nakryiko <andrii@kernel.org>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Christophe Leroy <christophe.leroy@csgroup.eu>
    Cc: David Hildenbrand <david@redhat.com>
    Cc: Liam Howlett <liam.howlett@oracle.com>
    Cc: Mark Brown <broonie@kernel.org>
    Cc: Oscar Salvador <osalvador@suse.de>
    Cc: wangfushuai <wangfushuai@baidu.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -991,10 +991,7 @@ static void show_smap_vma_flags(struct seq_file *m, struct vm_area_struct *vma)
 #ifdef CONFIG_HAVE_ARCH_USERFAULTFD_MINOR
 		[ilog2(VM_UFFD_MINOR)]	= "ui",
 #endif /* CONFIG_HAVE_ARCH_USERFAULTFD_MINOR */
-#ifdef CONFIG_ARCH_HAS_USER_SHADOW_STACK
-		[ilog2(VM_SHADOW_STACK)] = "ss",
-#endif
-#if defined(CONFIG_ARM64_GCS)
+#if defined(CONFIG_ARCH_HAS_USER_SHADOW_STACK) || defined (CONFIG_ARM64_GCS)
 		[ilog2(VM_SHADOW_STACK)] = "ss",
 #endif
 #if defined(CONFIG_64BIT) || defined(CONFIG_PPC32)
