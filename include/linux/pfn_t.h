f823f30e520fbba34278ce22bd0cf79c92445fd9
Alistair Popple <apopple@nvidia.com>
2025-06-16 21:58:06 +1000

    mm: remove remaining uses of PFN_DEV
    PFN_DEV was used by callers of dax_direct_access() to figure out if the
    returned PFN is associated with a page using pfn_t_has_page() or not. 
    However all DAX PFNs now require an assoicated ZONE_DEVICE page so can
    assume a page exists.
    
    Other users of PFN_DEV were setting it before calling vmf_insert_mixed(). 
    This is unnecessary as it is no longer checked, instead relying on
    pfn_valid() to determine if there is an associated page or not.
    
    Link: https://lkml.kernel.org/r/059a2e87240d7bb00016155ef5426c08977759e0.1750075065.git-series.apopple@nvidia.com
    Signed-off-by: Alistair Popple <apopple@nvidia.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Jason Gunthorpe <jgg@nvidia.com>
    Reviewed-by: Dan Williams <dan.j.williams@intel.com>
    
    Cc: Balbir Singh <balbirs@nvidia.com>
    Cc: Björn Töpel <bjorn@kernel.org>
    Cc: Björn Töpel <bjorn@rivosinc.com>
    Cc: Chunyan Zhang <zhang.lyra@gmail.com>
    Cc: David Hildenbrand <david@redhat.com>
    Cc: Deepak Gupta <debug@rivosinc.com>
    Cc: Gerald Schaefer <gerald.schaefer@linux.ibm.com>
    Cc: Inki Dae <m.szyprowski@samsung.com>
    Cc: John Groves <john@groves.net>
    Cc: John Hubbard <jhubbard@nvidia.com>
    Cc: Lorenzo Stoakes <lorenzo.stoakes@oracle.com>
    Cc: Matthew Wilcox (Oracle) <willy@infradead.org>
    Cc: Will Deacon <will@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- include/linux/pfn_t.h
+++ include/linux/pfn_t.h
@@ -7,7 +7,6 @@
  * PFN_FLAGS_MASK - mask of all the possible valid pfn_t flags
  * PFN_SG_CHAIN - pfn is a pointer to the next scatterlist entry
  * PFN_SG_LAST - pfn references a page and is the last scatterlist entry
- * PFN_DEV - pfn is not covered by system memmap by default
  * PFN_MAP - pfn has a dynamic page mapping established by a device driver
  * PFN_SPECIAL - for CONFIG_FS_DAX_LIMITED builds to allow XIP, but not
  *		 get_user_pages
@@ -15,7 +14,6 @@
 #define PFN_FLAGS_MASK (((u64) (~PAGE_MASK)) << (BITS_PER_LONG_LONG - PAGE_SHIFT))
 #define PFN_SG_CHAIN (1ULL << (BITS_PER_LONG_LONG - 1))
 #define PFN_SG_LAST (1ULL << (BITS_PER_LONG_LONG - 2))
-#define PFN_DEV (1ULL << (BITS_PER_LONG_LONG - 3))
 #define PFN_MAP (1ULL << (BITS_PER_LONG_LONG - 4))
 #define PFN_SPECIAL (1ULL << (BITS_PER_LONG_LONG - 5))
 
@@ -23,7 +21,6 @@
 	{ PFN_SPECIAL,	"SPECIAL" }, \
 	{ PFN_SG_CHAIN,	"SG_CHAIN" }, \
 	{ PFN_SG_LAST,	"SG_LAST" }, \
-	{ PFN_DEV,	"DEV" }, \
 	{ PFN_MAP,	"MAP" }
 
 static inline pfn_t __pfn_to_pfn_t(unsigned long pfn, u64 flags)
@@ -46,7 +43,7 @@ static inline pfn_t phys_to_pfn_t(phys_addr_t addr, u64 flags)
 
 static inline bool pfn_t_has_page(pfn_t pfn)
 {
-	return (pfn.val & PFN_MAP) == PFN_MAP || (pfn.val & PFN_DEV) == 0;
+	return (pfn.val & PFN_MAP) == PFN_MAP;
 }
 
 static inline unsigned long pfn_t_to_pfn(pfn_t pfn)
@@ -100,7 +97,7 @@ static inline pud_t pfn_t_pud(pfn_t pfn, pgprot_t pgprot)
 #ifdef CONFIG_ARCH_HAS_PTE_DEVMAP
 static inline bool pfn_t_devmap(pfn_t pfn)
 {
-	const u64 flags = PFN_DEV|PFN_MAP;
+	const u64 flags = PFN_MAP;
 
 	return (pfn.val & flags) == flags;
 }
