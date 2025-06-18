58b059ddbfdde8765288265bb5849326f0e81977
Tim Chen <tim.c.chen@linux.intel.com>
2025-06-18 11:28:07 -0700

    sched: Introduce SCHED_CACHE_LB to control cache aware load balance
    Introduce the SCHED_CACHE_LB sched feature to enable or disable
    cache aware load balance in the schduler.
    
    Co-developed-by: Chen Yu <yu.c.chen@intel.com>
    Signed-off-by: Tim Chen <tim.c.chen@linux.intel.com>
    Link: https://lore.kernel.org/r/ac0e58b587dcd1e3ebe7a7a97973c5763d02a5f8.1750268218.git.tim.c.chen@linux.intel.com

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -88,6 +88,7 @@ SCHED_FEAT(TTWU_QUEUE, true)
 SCHED_FEAT(SIS_UTIL, true)
 
 SCHED_FEAT(SCHED_CACHE, true)
+SCHED_FEAT(SCHED_CACHE_LB, true)
 /*
  * Issue a WARN when we do multiple update_rq_clock() calls
  * in a single rq->lock section. Default disabled because the
