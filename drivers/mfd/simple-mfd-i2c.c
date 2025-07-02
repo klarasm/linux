b7a3c503d3b5a1048c5af98a9db7c2f1353a8763
Alex Elder <elder@riscstar.com>
2025-07-02 16:06:20 -0500

    mfd: simple-mfd-i2c: specify max_register
    All devices supported by simple MFD use the same 8-bit register
    8-bit value regmap configuration.  There is an option available
    for a device to specify a custom configuration, but no existing
    device uses it.
    
    Rather than specify a "full" regmap configuration to use this
    option, Lee Jones suggested allowing just the max_register value
    to be specified in the simple_mfd_data structure.
    
    Signed-off-by: Alex Elder <elder@riscstar.com>
    Suggested-by: Lee Jones <lee@kernel.org>

--- drivers/mfd/simple-mfd-i2c.c
+++ drivers/mfd/simple-mfd-i2c.c
@@ -33,16 +33,25 @@ static int simple_mfd_i2c_probe(struct i2c_client *i2c)
 {
 	const struct simple_mfd_data *simple_mfd_data;
 	const struct regmap_config *regmap_config;
+	struct regmap_config config;
 	struct regmap *regmap;
 	int ret;
 
 	simple_mfd_data = device_get_match_data(&i2c->dev);
 
 	/* If no regmap_config is specified, use the default 8reg and 8val bits */
-	if (!simple_mfd_data || !simple_mfd_data->regmap_config)
+	if (simple_mfd_data) {
+		if (simple_mfd_data->regmap_config)
+			config = *simple_mfd_data->regmap_config;
+		else
+			config = regmap_config_8r_8v;
+
+		if (simple_mfd_data->max_register)
+			config.max_register = simple_mfd_data->max_register;
+		regmap_config = &config;
+	} else {
 		regmap_config = &regmap_config_8r_8v;
-	else
-		regmap_config = simple_mfd_data->regmap_config;
+	}
 
 	regmap = devm_regmap_init_i2c(i2c, regmap_config);
 	if (IS_ERR(regmap))
