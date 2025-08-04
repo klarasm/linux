0c80377110bc65fd5813a79b16bdcf381912e2b5
Kairui Song <kasong@tencent.com>
2025-08-04 16:28:35 +0800

    mm, swap: drop entry as parameter for swap cgroup charging

--- mm/swapfile.c
+++ mm/swapfile.c
@@ -1622,7 +1622,7 @@ int folio_alloc_swap(struct folio *folio, gfp_t gfp)
 		return -ENOMEM;
 	}
 
-	if (mem_cgroup_try_charge_swap(folio, folio->swap)) {
+	if (mem_cgroup_try_charge_swap(folio)) {
 		swap_cache_del_folio(folio);
 		return -ENOMEM;
 	}
