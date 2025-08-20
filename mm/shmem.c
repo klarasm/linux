2be75ce269717a2bf07f1cc532475ebc8734d628
Baolin Wang <baolin.wang@linux.alibaba.com>
2025-08-20 17:07:16 +0800

    mm: shmem: kick khugepaged for enabling none-PMD-sized shmem mTHPs
    When only non-PMD-sized mTHP is enabled (such as only 64K mTHP enabled),
    we should also allow kicking khugepaged to attempt scanning and collapsing
    64K shmem mTHP. Modify shmem_hpage_pmd_enabled() to support shmem mTHP
    collapse, and while we are at it, rename it to make the function name
    more clear.
    
    Signed-off-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Link: https://lore.kernel.org/r/7c8ee99eb146a9e8abd20d110cb591d33fa1ebae.1755677674.git.baolin.wang@linux.alibaba.com

--- mm/shmem.c
+++ mm/shmem.c
@@ -1791,17 +1791,17 @@ static gfp_t limit_gfp_mask(gfp_t huge_gfp, gfp_t limit_gfp)
 }
 
 #ifdef CONFIG_TRANSPARENT_HUGEPAGE
-bool shmem_hpage_pmd_enabled(void)
+bool shmem_hpage_enabled(void)
 {
 	if (shmem_huge == SHMEM_HUGE_DENY)
 		return false;
-	if (test_bit(HPAGE_PMD_ORDER, &huge_shmem_orders_always))
+	if (READ_ONCE(huge_shmem_orders_always))
 		return true;
-	if (test_bit(HPAGE_PMD_ORDER, &huge_shmem_orders_madvise))
+	if (READ_ONCE(huge_shmem_orders_madvise))
 		return true;
-	if (test_bit(HPAGE_PMD_ORDER, &huge_shmem_orders_within_size))
+	if (READ_ONCE(huge_shmem_orders_within_size))
 		return true;
-	if (test_bit(HPAGE_PMD_ORDER, &huge_shmem_orders_inherit) &&
+	if (READ_ONCE(huge_shmem_orders_inherit) &&
 	    shmem_huge != SHMEM_HUGE_NEVER)
 		return true;
 
