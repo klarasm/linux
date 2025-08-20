f359efd76de0ed99e8f5cea55c4562638bae3821
Baolin Wang <baolin.wang@linux.alibaba.com>
2025-08-20 17:07:20 +0800

    selftests: mm: move gather_after_split_folio_orders() into vm_util.c file
    Move gather_after_split_folio_orders() to vm_util.c as a helper function
    in preparation for implementing checks for mTHP collapse. While we are
    at it, rename this function to indicate that it is not only used for
    large folio splits.
    
    No functional changes.
    
    Signed-off-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Link: https://lore.kernel.org/r/955e0b9682b1746c528a043f0ca530b54ee22536.1755677674.git.baolin.wang@linux.alibaba.com

--- tools/testing/selftests/mm/vm_util.h
+++ tools/testing/selftests/mm/vm_util.h
@@ -89,6 +89,8 @@ int64_t allocate_transhuge(void *ptr, int pagemap_fd);
 unsigned long default_huge_page_size(void);
 int detect_hugetlb_page_sizes(size_t sizes[], int max);
 int pageflags_get(unsigned long pfn, int kpageflags_fd, uint64_t *flags);
+int gather_folio_orders(char *vaddr_start, size_t len,
+		int pagemap_fd, int kpageflags_fd, int orders[], int nr_orders);
 
 int uffd_register(int uffd, void *addr, uint64_t len,
 		  bool miss, bool wp, bool minor);
