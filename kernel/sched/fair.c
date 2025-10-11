5edb25914094c8045342e3dcb6176863381c516c
Tim Chen <tim.c.chen@linux.intel.com>
2025-10-11 11:24:43 -0700

    sched/fair: Assign preferred LLC ID to processes
    With cache-aware scheduling enabled, each task is assigned a
    preferred LLC ID. This allows quick identification of the LLC domain
    where the task prefers to run, similar to numa_preferred_nid in
    NUMA balancing.
    
    Signed-off-by: Tim Chen <tim.c.chen@linux.intel.com>
    Message-ID: <cfa266cd6ea6fa30cbf7b07573992f18f786955e.1760206683.git.tim.c.chen@linux.intel.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -1312,6 +1312,7 @@ void account_mm_sched(struct rq *rq, struct task_struct *p, s64 delta_exec)
 	struct mm_struct *mm = p->mm;
 	struct mm_sched *pcpu_sched;
 	unsigned long epoch;
+	int mm_sched_llc = -1;
 
 	if (!sched_cache_enabled())
 		return;
@@ -1342,6 +1343,12 @@ void account_mm_sched(struct rq *rq, struct task_struct *p, s64 delta_exec)
 		if (mm->mm_sched_cpu != -1)
 			mm->mm_sched_cpu = -1;
 	}
+
+	if (mm->mm_sched_cpu != -1)
+		mm_sched_llc = per_cpu(sd_llc_id, mm->mm_sched_cpu);
+
+	if (p->preferred_llc != mm_sched_llc)
+		p->preferred_llc = mm_sched_llc;
 }
 
 static void task_tick_cache(struct rq *rq, struct task_struct *p)
