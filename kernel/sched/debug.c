2415734095ccff55e4704e30b418d9c17572109b
Chen Yu <yu.c.chen@intel.com>
2025-10-11 11:24:40 -0700

    sched/fair: Introduce helper functions to enforce LLC migration policy
    Cache-aware scheduling aggregates threads onto their preferred LLC,
    mainly through load balancing. When the preferred LLC becomes
    saturated, more threads are still placed there, increasing latency.
    A mechanism is needed to limit aggregation so that the preferred LLC
    does not become overloaded.
    
    Introduce helper functions can_migrate_llc() and
    can_migrate_llc_task() to enforce the LLC migration policy:
    
      1. Aggregate a task to its preferred LLC if both source and
         destination LLCs are not too busy (<50% utilization, tunable),
         or if doing so will not leave the preferred LLC much more
         imbalanced than the non-preferred one (>20% utilization
         difference, tunable, similar to imbalance_pct of the LLC domain).
      2. Allow moving a task from overloaded preferred LLC to a non preferred
         LLC if this will not cause the non preferred LLC to become
         too imbalanced to cause a later migration back.
      3. If both LLCs are too busy, let the generic load balance to spread
         the tasks.
    
    This hysteresis prevents tasks from being migrated into and out of the
    preferred LLC frequently (back and forth): the threshold for migrating
    a task out of its preferred LLC is higher than that for migrating it
    into the LLC.
    
    Since aggregation tends to make the preferred LLC busier than others,
    the imbalance tolerance is controlled by llc_imb_pct. If set to 0,
    tasks may still aggregate to the preferred LLC as long as it is
    not more utilized than the source LLC, preserving the preference.
    
    Co-developed-by: Tim Chen <tim.c.chen@linux.intel.com>
    Signed-off-by: Tim Chen <tim.c.chen@linux.intel.com>
    Signed-off-by: Chen Yu <yu.c.chen@intel.com>
    Message-ID: <d6830774db8e260af4da728c44e9f899376a0ea6.1760206683.git.tim.c.chen@linux.intel.com>

--- kernel/sched/debug.c
+++ kernel/sched/debug.c
@@ -523,6 +523,10 @@ static __init int sched_init_debug(void)
 	debugfs_create_u32("hot_threshold_ms", 0644, numa, &sysctl_numa_balancing_hot_threshold);
 #endif /* CONFIG_NUMA_BALANCING */
 
+#ifdef CONFIG_SCHED_CACHE
+	debugfs_create_u32("llc_overload_pct", 0644, debugfs_sched, &llc_overload_pct);
+	debugfs_create_u32("llc_imb_pct", 0644, debugfs_sched, &llc_imb_pct);
+#endif
 	debugfs_create_file("debug", 0444, debugfs_sched, NULL, &sched_debug_fops);
 
 	debugfs_fair_server_init();
