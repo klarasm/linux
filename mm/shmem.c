34ae5e78583115bd223639c608a9e5734bd867d6
Luiz Capitulino <luizcap@redhat.com>
2025-12-15 16:16:49 -0500

    mm: shmem: use pgtable_has_pmd_leaves()
    Shmem uses has_transparent_hugepage() to check if PMD-sized pages are
    supported, use pgtable_has_pmd_leaves() instead.
    
    Signed-off-by: Luiz Capitulino <luizcap@redhat.com>
    Message-ID: <e2eab9e249d10ea559e1e3c9e9b224bf2822d13e.1765833318.git.luizcap@redhat.com>

--- mm/shmem.c
+++ mm/shmem.c
@@ -689,7 +689,8 @@ static int shmem_parse_huge(const char *str)
 	else
 		return -EINVAL;
 
-	if (!has_transparent_hugepage() &&
+	if (!(IS_ENABLED(CONFIG_TRANSPARENT_HUGEPAGE) &&
+	      pgtable_has_pmd_leaves()) &&
 	    huge != SHMEM_HUGE_NEVER && huge != SHMEM_HUGE_DENY)
 		return -EINVAL;
 
@@ -4655,7 +4656,7 @@ static int shmem_parse_one(struct fs_context *fc, struct fs_parameter *param)
 		ctx->huge = result.uint_32;
 		if (ctx->huge != SHMEM_HUGE_NEVER &&
 		    !(IS_ENABLED(CONFIG_TRANSPARENT_HUGEPAGE) &&
-		      has_transparent_hugepage()))
+		      pgtable_has_pmd_leaves()))
 			goto unsupported_parameter;
 		ctx->seen |= SHMEM_SEEN_HUGE;
 		break;
@@ -5439,7 +5440,7 @@ void __init shmem_init(void)
 #endif
 
 #ifdef CONFIG_TRANSPARENT_HUGEPAGE
-	if (has_transparent_hugepage() && shmem_huge > SHMEM_HUGE_DENY)
+	if (pgtable_has_pmd_leaves() && shmem_huge > SHMEM_HUGE_DENY)
 		SHMEM_SB(shm_mnt->mnt_sb)->huge = shmem_huge;
 	else
 		shmem_huge = SHMEM_HUGE_NEVER; /* just in case it was patched */
