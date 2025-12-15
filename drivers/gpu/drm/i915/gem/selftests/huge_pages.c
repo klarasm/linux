ad5112d519fe2c24a3ec780c8b099c94c0ac210f
Luiz Capitulino <luizcap@redhat.com>
2025-12-15 16:16:46 -0500

    drivers: i915 selftest: use pgtable_has_pmd_leaves()
    igt_can_allocate_thp() uses has_transparent_hugepage() to check if
    PMD-sized pages are supported, use pgtable_has_pmd_leaves() instead.
    
    Since igt_can_allocate_thp() wants to use PMD-sized pages with THP,
    also check if THP is built-in with IS_ENABLED(CONFIG_TRANSPARENT_HUGEPAGE).
    
    Signed-off-by: Luiz Capitulino <luizcap@redhat.com>
    Message-ID: <db6f2106e8925fd3e037ae95c2eebe9b3410d7d7.1765833318.git.luizcap@redhat.com>

--- drivers/gpu/drm/i915/gem/selftests/huge_pages.c
+++ drivers/gpu/drm/i915/gem/selftests/huge_pages.c
@@ -1316,7 +1316,9 @@ typedef struct drm_i915_gem_object *
 
 static inline bool igt_can_allocate_thp(struct drm_i915_private *i915)
 {
-	return i915->mm.gemfs && has_transparent_hugepage();
+	return i915->mm.gemfs &&
+		IS_ENABLED(CONFIG_TRANSPARENT_HUGEPAGE) &&
+		pgtable_has_pmd_leaves();
 }
 
 static struct drm_i915_gem_object *
