6c554930fde3e83033ad27c2eb24b4965778a1c4
Peter Zijlstra (Intel) <peterz@infradead.org>
2026-02-10 14:18:41 -0800

    sched/cache: Introduce infrastructure for cache-aware load balancing
    Adds infrastructure to enable cache-aware load balancing,
    which improves cache locality by grouping tasks that share resources
    within the same cache domain. This reduces cache misses and improves
    overall data access efficiency.
    
    In this initial implementation, threads belonging to the same process
    are treated as entities that likely share working sets. The mechanism
    tracks per-process CPU occupancy across cache domains and attempts to
    migrate threads toward cache-hot domains where their process already
    has active threads, thereby enhancing locality.
    
    This provides a basic model for cache affinity. While the current code
    targets the last-level cache (LLC), the approach could be extended to
    other domain types such as clusters (L2) or node-internal groupings.
    
    At present, the mechanism selects the CPU within an LLC that has the
    highest recent runtime. Subsequent patches in this series will use this
    information in the load-balancing path to guide task placement toward
    preferred LLCs.
    
    In the future, more advanced policies could be integrated through NUMA
    balancing-for example, migrating a task to its preferred LLC when spare
    capacity exists, or swapping tasks across LLCs to improve cache affinity.
    Grouping of tasks could also be generalized from that of a process
    to be that of a NUMA group, or be user configurable.
    
    Originally-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Tim Chen <tim.c.chen@linux.intel.com>
    Signed-off-by: Chen Yu <yu.c.chen@intel.com>
    Message-ID: <6ec6eee6e1c620c0cfb9f56923f8bfbb71c31a75.1770760558.git.tim.c.chen@linux.intel.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1409,6 +1409,10 @@ struct task_struct {
 	unsigned long			numa_pages_migrated;
 #endif /* CONFIG_NUMA_BALANCING */
 
+#ifdef CONFIG_SCHED_CACHE
+	struct callback_head		cache_work;
+#endif
+
 	struct rseq_data		rseq;
 	struct sched_mm_cid		mm_cid;
 
@@ -2328,6 +2332,26 @@ static __always_inline int task_mm_cid(struct task_struct *t)
 	 */
 	return task_cpu(t);
 }
+#endif
+
+#ifdef CONFIG_SCHED_CACHE
+
+struct sched_cache_time {
+	u64 runtime;
+	unsigned long epoch;
+};
+
+struct sched_cache_stat {
+	struct sched_cache_time __percpu *pcpu_sched;
+	raw_spinlock_t lock;
+	unsigned long epoch;
+	int cpu;
+} ____cacheline_aligned_in_smp;
+
+#else
+
+struct sched_cache_stat { };
+
 #endif
 
 #ifndef MODULE
