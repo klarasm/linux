d6bb6f18f00db3be1edc0ce480c4a9b4a69e39f3
Usama Arif <usama.arif@linux.dev>
2026-03-26 19:08:58 -0700

    mm: huge_mm: Make sure all split_huge_pmd calls are checked
    Mark __split_huge_pmd(), split_huge_pmd() and split_huge_pmd_address()
    with __must_check so the compiler warns if any caller ignores the return
    value. Not checking return value and operating on the basis that the pmd
    is split could result in a kernel bug. The possibility of an order-0
    allocation failing for page table allocation is very low, but it should
    be handled correctly.
    
    Signed-off-by: Usama Arif <usama.arif@linux.dev>
    Message-ID: <20260327021403.214713-17-usama.arif@linux.dev>

--- include/linux/huge_mm.h
+++ include/linux/huge_mm.h
@@ -424,7 +424,7 @@ void deferred_split_folio(struct folio *folio, bool partially_mapped);
 void reparent_deferred_split_queue(struct mem_cgroup *memcg);
 #endif
 
-int __split_huge_pmd(struct vm_area_struct *vma, pmd_t *pmd,
+int __must_check __split_huge_pmd(struct vm_area_struct *vma, pmd_t *pmd,
 		unsigned long address, bool freeze);
 
 /**
@@ -453,7 +453,7 @@ static inline bool pmd_is_huge(pmd_t pmd)
 	return false;
 }
 
-static inline int split_huge_pmd(struct vm_area_struct *vma,
+static inline int __must_check split_huge_pmd(struct vm_area_struct *vma,
 					     pmd_t *pmd, unsigned long address)
 {
 	if (pmd_is_huge(*pmd))
@@ -461,7 +461,7 @@ static inline int split_huge_pmd(struct vm_area_struct *vma,
 	return 0;
 }
 
-int split_huge_pmd_address(struct vm_area_struct *vma, unsigned long address,
+int __must_check split_huge_pmd_address(struct vm_area_struct *vma, unsigned long address,
 		bool freeze);
 
 void __split_huge_pud(struct vm_area_struct *vma, pud_t *pud,
